387ae5e8fec0b7508a3767a84d68035cbe0112c7
Daniel-Davis <ddv9461@saintjoe.edu>
2015-04-01 12:42:35 -0500

    Update string.js

--- string.js
+++ string.js
@@ -1,2 +1,6 @@
 // Daniel Davis, string, answers question 3 of the practicum section //
 
+var string = prompt("please enter a string of characters"); // gets user input for string output
+var int = prompt("please enter a number"); // gets user input for number output
+
+print("You entered " + string + " for your string input, and " + int + " for your number.") // prints out the input of the user
