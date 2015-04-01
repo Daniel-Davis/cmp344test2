f7928871e730bb8581ffe1854ed6afbc70127e8a
Daniel-Davis <ddv9461@saintjoe.edu>
2015-04-01 12:35:43 -0500

    Update array.js

--- array.js
+++ array.js
@@ -1,2 +1,9 @@
 // Daniel Davis, array, answers question 1 of the javascript 101 practicum section //
 
+var arr = []; // creates an array to hold 20 values
+
+for (var i=0, i < 20, ++i) { // for loop to generate 20 random numbers
+  arr[i] = Math.floor((Math.random() * 200) + 10); // numbers are between 10 and 200
+}// end of for loop
+
+print(arr); // prints out the array
